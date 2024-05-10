e61b3a2070cc884a2a2f4fcec4c4a2ad6105e58f
maxigeist <geist.maximo@gmail.com>
2024-05-10 16:54:16 -0300

    feat: add the linter configurations

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,23 +1,31 @@
-import React from "react";
-import "./styles.css";
-import "@/app/globals.css";
+import React from 'react';
+import './styles.css';
+import '@/app/globals.css';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: React.ReactNode;
-    variant?: "grey" | "green" | "red";
-    size?: "small" | "medium" | "large";
+    variant?: 'grey' | 'green' | 'red';
+    size?: 'small' | 'medium' | 'large';
     onClick?: () => void;
     disabled?: boolean;
 }
 
-const Button = ({children, variant, size, onClick, disabled}: ButtonProps) => {
+const Button = ({
+    children,
+    variant,
+    size,
+    onClick,
+    disabled,
+}: ButtonProps) => {
     return (
-        <button onClick={onClick}
-                className={"button body2bold" + " " + variant + " " + size}
-                disabled={disabled}>
+        <button
+            onClick={onClick}
+            className={'button body2bold' + ' ' + variant + ' ' + size}
+            disabled={disabled}
+        >
             {children}
         </button>
-    )
+    );
 }
 
 export default Button;
