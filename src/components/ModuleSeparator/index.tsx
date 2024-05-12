56cb91ba146a735d609a850dcf774128d75de3f0
santi <santiago.peres@ing.austral.edu.ar>
2024-05-12 19:54:13 -0300

    module separator component

---
+++ src/components/ModuleSeparator/index.tsx
@@ -0,0 +1,36 @@
+import { Box, Typography } from '@mui/material';
+import React from 'react';
+
+const ModuleSeparator = ({ text }: { text: string }) => {
+    return (
+        <Box
+            display={'flex'}
+            alignItems={'center'}
+            justifyContent={'space-between'}
+        >
+            <Box
+                sx={{
+                    backgroundColor: 'var(--grey-400)',
+                    height: '0.4vh',
+                    width: '25%',
+                }}
+            ></Box>
+            <Typography
+                className="h5 bold"
+                color="var(--grey-400)"
+                align="center"
+            >
+                {text}
+            </Typography>
+            <Box
+                sx={{
+                    backgroundColor: 'var(--grey-400)',
+                    height: '0.4vh',
+                    width: '25%',
+                }}
+            ></Box>
+        </Box>
+    );
+};
+
+export default ModuleSeparator;
