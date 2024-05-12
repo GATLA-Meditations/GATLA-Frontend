182f1472309239081ce379f683469bb22cc628f6
AAugurusa <95092029+AAugurusa@users.noreply.github.com>
2024-05-12 15:25:22 -0300

    Creates new navbar using MUI, with buttons, Home, Meet and User

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,21 @@
+import Button from '@/components/Button';
+import TopBar from '@/components/TopBar';
+import NavBar from '@/components/NavBar';
+
 const HomeScreen = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div >
+            <TopBar amtNotifications={5} />
+            <Button variant="red" size="medium">
+                Click me!
+            </Button>
+            <Button variant="red" size="large">
+                Click me!
+            </Button>
+            <Button variant="red" size="small">
+                Click me!
+            </Button>
+            <NavBar value={0}/>
         </div>
     );
 };
