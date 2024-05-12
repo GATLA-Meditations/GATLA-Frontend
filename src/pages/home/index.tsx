6df9a7ea6b7ad3a33217f737f3e889e9395448d8
AAugurusa <95092029+AAugurusa@users.noreply.github.com>
2024-05-12 18:48:20 -0300

    GTL-108 Creates a question component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,10 @@
+import Question from '@/components/Question';
+
 const HomeScreen = () => {
     return (
         <div>
             <h1>Home</h1>
+            <Question questionNumber={1} questionText={'Tu felicidad esta directamente proporcionada por tus bienes materiales'}/>
         </div>
     );
 };
