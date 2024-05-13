fb3c553ee1a584c93b3e3d9dbb44e7eea062a85a
maxigeist <geist.maximo@gmail.com>
2024-05-13 19:43:05 -0300

    changed to css style in the Box tag in reward card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import RewardCard from '@/components/RewardCard';
+import { FireIcon } from '@/assets/FireIcon';
+
 export default function Home() {
     return (
         <main>
             <h1>hello world</h1>
+            <RewardCard icon={<FireIcon color={'#F19384'} />} text={'1 DÍA'} />
         </main>
     );
 }
