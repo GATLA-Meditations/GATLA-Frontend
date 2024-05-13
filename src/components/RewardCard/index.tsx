cdb58b4f6002d931442358d1849c137359283486
maxigeist <geist.maximo@gmail.com>
2024-05-13 19:02:39 -0300

    feat: add the Reward Card component and the two different logos that the card has in assets

---
+++ src/components/RewardCard/index.tsx
@@ -0,0 +1,37 @@
+import Box from '@mui/material/Box';
+import React from 'react';
+import Typography from '@mui/material/Typography';
+
+interface RewardCardProps {
+    logo: React.ReactNode;
+    text: string;
+    bgColor: string;
+    textColor: string;
+}
+
+const RewardCard = ({ logo, text, bgColor, textColor }: RewardCardProps) => {
+    return (
+        <Box
+            width={'80px'}
+            height={'67px'}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="column"
+            gap={'6px'}
+            bgcolor={bgColor}
+            padding={'8px 3px'}
+            borderRadius={'10px'}
+            boxSizing={'content-box'}
+            boxShadow={'0px 4px 4px rgba(0, 0, 0, 0.25)'}
+        >
+            <Box height={40} width={40}>
+                {logo}
+            </Box>
+            <Box>
+                <Typography color={textColor} className={'body2 bold'}>{text}</Typography>
+            </Box>
+        </Box>
+    );
+};
+export default RewardCard;
