f7006fad25646ccb2e7689d74b60f9beb831f5b6
franciscocavallaro <ccavallaro00@gmail.com>
2024-05-14 17:45:06 -0300

    Added to be able to change color of the text and the separator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import ModuleSeparator from '@/components/ModuleSeparator';
+
 export default function Home() {
     return (
         <main>
             <h1>hello world</h1>
+            <ModuleSeparator text={'hello world'} textColor={'blue'} separatorColor={'black'} />
         </main>
     );
 }
--- src/components/ModuleSeparator/index.tsx
+++ src/components/ModuleSeparator/index.tsx
@@ -1,25 +1,31 @@
 import { Box, Divider, Typography } from '@mui/material';
 import React from 'react';
 
-const ModuleSeparator = ({ text }: { text: string }) => {
+interface ModuleSeparatorProps {
+    text: string;
+    separatorColor: string;
+    textColor: string;
+}
+
+const ModuleSeparator = ({ text, separatorColor, textColor }: ModuleSeparatorProps) => {
     return (
         <Divider
             sx={{
                 '&.MuiDivider-root::before': {
                     width: '100%',
                     borderTop: 'solid',
-                    color: 'var(--grey-400)',
+                    color: separatorColor,
                 },
                 '&.MuiDivider-root::after': {
                     width: '100%',
                     borderTop: 'solid',
-                    color: 'var(--grey-400)',
+                    color: separatorColor,
                 },
             }}
         >
             <Typography
                 className="h5 bold"
-                color="var(--grey-400)"
+                color={textColor}
                 align="center"
             >
                 {text}
