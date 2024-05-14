4521c5549cc9d081bee0824290fcd4c2e4d76035
franciscocavallaro <ccavallaro00@gmail.com>
2024-05-14 17:45:50 -0300

    Deleted separator in the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import ModuleSeparator from '@/components/ModuleSeparator';
-
 export default function Home() {
     return (
         <main>
             <h1>hello world</h1>
-            <ModuleSeparator text={'hello world'} textColor={'blue'} separatorColor={'black'} />
         </main>
     );
 }
