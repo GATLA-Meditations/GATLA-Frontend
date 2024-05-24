93d178d35179e140864394f057ad6dc5c0a2639f
AAugurusa <95092029+AAugurusa@users.noreply.github.com>
2024-05-24 17:53:47 -0300

    GTL-130 Creates home page with module section

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,15 +5,13 @@ import './styles.css';
 import MeditationEntryPoint from '@/components/MeditationEntryPoint';
 import NavBar from '@/components/NavBar';
 import Box from '@mui/material/Box';
+import AchievementsHomeMenu from '@/components/AchievementsHomeMenu';
 
 const HomeScreen = () => {
     return (
-        <Box
-            className="component"
-            style={{ backgroundColor: 'var(--bg-color)' }}
-        >
+        <Box height={'100vh'} style={{ backgroundColor: 'var(--bg-color)' }}>
             <TopBar amtNotifications={0} selected={''} />
-            <p>stats</p>
+            <AchievementsHomeMenu days={1} minutes={1} goals={1} />
             <ModuleSeparator
                 text={'Módulo actual'}
                 separatorColor={'#141418'}
