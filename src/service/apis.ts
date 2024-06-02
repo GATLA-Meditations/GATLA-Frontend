2efa6b4876b835b40624b0229b831a8fd289c84d
franciscocavallaro <ccavallaro00@gmail.com>
2024-06-02 17:23:11 -0300

    Axios set up done

---
+++ src/service/apis.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const baseURL = 'http://localhost:3001';
+
+const config = (token: string) => ({
+    headers: {
+        Authorization: `Bearer ${token}`,
+    },
+});
+
+const gatlaAxios = axios.create({
+    baseURL,
+});
+
+gatlaAxios.interceptors.response.use(
+    function (response) {
+        return response;
+    },
+    function (error) {
+        if (error.response.status === 401 || error.response.status === 403) {
+            localStorage.clear();
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
+
+
