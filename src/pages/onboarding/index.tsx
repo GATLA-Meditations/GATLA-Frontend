aeb220b80508ffcb9248916ac52012013fd747eb
maxigeist <geist.maximo@gmail.com>
2024-06-02 13:48:36 -0300

    started onboarding-screen

--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -1,32 +1,44 @@
-import './test.css';
-import Button from '@/components/Button';
 import { useRouter } from 'next/router';
+import HomeScreen from '@/pages/home';
+import './styles.css';
+import TopBar from '@/components/TopBar';
+import { useEffect, useState } from 'react';
 
 const TestPageOnboarding = () => {
+    const [state, setState] = useState('topbar');
     const router = useRouter();
-    const goToTest = () => {
-        // @ts-ignore
-        router.push('/test').then(() => console.log('click'));
-    };
 
-    return (
-        <div className={'TestPageOnboarding'}>
-            <div>
-                {/*<img src="https://via.placeholder.com/117x113" alt={'ssss'} />*/}
-                <h1>Antes de Meditar...</h1>
-                <h2>
-                    Quisieramos pedirle que complete este breve cuestionario con
-                    completa honestidad
-                </h2>
+    useEffect(() => {}, [state]);
+    if (state === 'topbar') {
+        return (
+            <div className={'TestPageOnboarding'}>
+                <div
+                    className={'black-screen'}
+                    onClick={() => setState('notifications')}
+                ></div>
+                <div className={'white-square'}>
+                    <TopBar amtNotifications={0} selected={state} />
+                </div>
+                <p className={'onboarding-text'}>Esto es la top bar</p>
+                <HomeScreen />
             </div>
-            <Button
-                variant={'green'}
-                size={'small'}
-                onClick={goToTest}
-                // children={<h3>Empezar</h3>}
-            />
-        </div>
-    );
+        );
+    }
+    if (state === 'notifications') {
+        return (
+            <div className={'TestPageOnboarding'}>
+                <div className={'black-screen'}></div>
+                <div className={'white-square'}>
+                    <TopBar amtNotifications={0} selected={state} />
+                </div>
+                <p className={'onboarding-text'}>
+                    Apretando aquí podrás dirigirte a la pantalla de
+                    notificaciones
+                </p>
+                {/*<HomeScreen />*/}
+            </div>
+        );
+    }
 };
 
 export default TestPageOnboarding;
