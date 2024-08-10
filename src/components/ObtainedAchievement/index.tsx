c61d4c55b00902adfb9676acea3c64bdaae9e5c3
Agustin Augurusa <augurusaagustin@gmail.com>
2024-08-09 22:19:46 -0300

    creates obtained achievement with aux logo for obtained achievement

---
+++ src/components/ObtainedAchievement/index.tsx
@@ -0,0 +1,28 @@
+import Box from '@mui/material/Box';
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import './styles.css';
+import Image, {StaticImageData} from 'next/image';
+
+interface ObtainedAchievementProps {
+    rewardCard: StaticImageData;
+    text?: string;
+    title?: string;
+}
+
+const ObtainedAchievement = ({rewardCard, text, title}: ObtainedAchievementProps) => {
+    return (
+        <Box className={'achievement-card'}>
+            <Box className={'image-container'}>
+                <Image src={rewardCard} alt="logo" style={{ objectFit: 'contain' }} width={100} height={100} />
+            </Box>
+            <Typography className={'title'}>
+                {title}
+            </Typography>
+            <Typography className={'text'}>
+                {text}
+            </Typography>
+        </Box>
+    );
+};
+export default ObtainedAchievement;
