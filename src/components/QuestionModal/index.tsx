3dce9e89f20c3f255f268375254afe44153add5d
franciscocavallaro <ccavallaro00@gmail.com>
2024-10-14 15:17:24 -0300

    Merged with maxos work

--- src/components/QuestionModal/index.tsx
+++ src/components/QuestionModal/index.tsx
@@ -1,28 +1,35 @@
-import {Modal, Typography} from '@mui/material';
+import { Modal, Slider, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import './styles.css';
 import Button from '@/components/Button';
-import {useState} from 'react';
+import React, { useState } from 'react';
+import { IWeeklyQuestion } from '@/components/QuestionModalManager';
 
 interface QuestionModalProps {
     open: boolean;
-    title:string;
+    title: string;
     questionIndex: number;
-    questionAmount:number;
-    question: string;
+    questionAmount: number;
+    question: IWeeklyQuestion;
     sendAnswerFunction: (index: number, value: string) => void;
 }
 
-
-const QuestionModal = ({open, title, questionIndex, questionAmount, question, sendAnswerFunction}: QuestionModalProps) => {
-
-
+const QuestionModal = ({
+    open,
+    title,
+    questionIndex,
+    questionAmount,
+    question,
+    sendAnswerFunction,
+}: QuestionModalProps) => {
     const [answer, setAnswerState] = useState('');
 
     const handleSendAnswer = () => {
         sendAnswerFunction(questionIndex, answer);
         setAnswerState('');
     };
+
+    const [sliderValue, setSliderValue] = useState(5);
 
     return (
         <Modal open={open} className={'question-modal'}>
@@ -38,31 +45,62 @@ const QuestionModal = ({open, title, questionIndex, questionAmount, question, se
                         Pregunta {questionIndex}/{questionAmount}
                     </Typography>
                 </Box>
-                <Box className={'question-modal-index-container'}>
-                    <Typography className={'h5'} textAlign={'center'}>
-                        {questionIndex}
-                    </Typography>
+                <Box className='question-container'>
+                    <Box className={'question-modal-index-container'}>
+                        <Typography className={'h5'} textAlign={'center'}>
+                            {questionIndex}
+                        </Typography>
+                    </Box>
+                    <Box className={'modal-question-container'}>
+                        <Typography textAlign={'center'} className={'body1'}>
+                            {question.text}
+                        </Typography>
+                    </Box>
+                    <Box style={{ width: '100%' }} className="chulo">
+                        {question.type === 'cuantitativa' ? (
+                            <Box>
+                                <Slider
+                                    defaultValue={5}
+                                    min={0}
+                                    max={10}
+                                    step={1}
+                                    onChange={(e, value) =>
+                                        setSliderValue(value as number)
+                                    }
+                                />
+                                <Typography
+                                    textAlign={'center'}
+                                    className={'body1'}
+                                >
+                                    Tu respuesta es: {sliderValue}
+                                </Typography>
+                            </Box>
+                        ) : (
+                            <Box className={'question-modal-input-container'}>
+                                <textarea
+                                    className={'question-modal-textarea'}
+                                    placeholder={'Escribí acá...'}
+                                    onChange={(e) =>
+                                        setAnswerState(e.target.value)
+                                    }
+                                    value={answer}
+                                />
+                            </Box>
+                        )}
+                    </Box>
+                    <Button
+                        variant={'common'}
+                        size={'medium'}
+                        onClick={() => handleSendAnswer()}
+                    >
+                        {questionIndex === questionAmount
+                            ? 'Enviar'
+                            : 'Siguiente'}
+                    </Button>
                 </Box>
-                <Box className={'modal-question-container'}>
-                    <Typography textAlign={'center'} className={'body1'}>
-                        {question}
-                    </Typography>
-                </Box>
-                <Box className={'question-modal-input-container'}>
-                    <textarea
-                        className={'question-modal-textarea'}
-                        placeholder={'Escribí acá...'}
-                        onChange={(e) => setAnswerState(e.target.value)}
-                        value={answer}
-                    />
-                </Box>
-                <Button variant={'common'} size={'medium'} onClick={() => handleSendAnswer()}>
-                    {questionIndex === questionAmount ? 'Enviar' : 'Siguiente'}
-                </Button>
             </Box>
         </Modal>
     );
-    
 };
 
 export default QuestionModal;
