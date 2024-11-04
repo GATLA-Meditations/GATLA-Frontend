91ec248ef1088f1defb9636979fd5b60f99b22eb
franciscocavallaro <ccavallaro00@gmail.com>
2024-11-04 12:29:42 -0300

    Helper modificado y agregado a personalizar

--- src/components/Help/index.tsx
+++ src/components/Help/index.tsx
@@ -1,15 +1,16 @@
 import './styles.css';
 import QuestionIcon from '@/assets/QuestionIcon';
-import {useState} from 'react';
+import { useState } from 'react';
 
 // The right and left props are optional and are used to position the help text
 export interface HelpProps {
     text: string;
-    right?:number;
-    left?:number;
+    right?: number;
+    left?: number;
+    color?: string;
 }
 
-const Help = ({ text, right, left }: HelpProps) => {
+const Help = ({ text, right, left, color }: HelpProps) => {
     const [isHelpOpen, setIsHelpOpen] = useState(false);
 
     const handleHelpClick = () => {
@@ -22,9 +23,18 @@ const Help = ({ text, right, left }: HelpProps) => {
                 <QuestionIcon width={'14px'} height={'14px'} color={'#FFF'} />
             </div>
             {isHelpOpen && (
-                <div className={'help-text-container'} style={{right: right, left: left}}>
+                <div
+                    className={'help-text-container'}
+                    style={{
+                        right: right,
+                        left: left,
+                        textAlign: 'center',
+                        backgroundColor: color,
+                    }}
+                >
                     <p>{text}</p>
-                </div>)}
+                </div>
+            )}
         </div>
     );
 };
